9eed1dc4638491334ab3bdb2c08d4cbfc8f9ed98
Arttanjeiro <arthursousarabelo2@gmail.com>
2024-07-09 16:00:32 -0300

    feat: criando componente de paticipante closes #11

---
+++ src/components/paticipant.tsx
@@ -0,0 +1,40 @@
+import { colors } from "@/styles/colors"
+import { CircleCheck, CircleDashed } from "lucide-react-native"
+import { Text, View } from "react-native"
+
+export interface ParticipantProps {
+  id: string
+  name?: string
+  email: string
+  isConfirmed: boolean
+}
+
+interface Props {
+  data: ParticipantProps
+}
+
+export function Participant({ data }: Props) {
+  return (
+    <View className="w-full flex-row items-center">
+      <View className="flex-1">
+        <Text className="text-zinc-100 text-base font-semibold">
+          {data.name ?? 'Pendente'}
+        </Text>
+
+        <Text className="text-zinc-400 text-sm">{data.email}</Text>
+      </View>
+
+      {data.isConfirmed ? (
+        <CircleCheck
+          color={colors.lime[300]}
+          size={20}
+        />
+      ) : (
+        <CircleDashed
+          color={colors.zinc[400]}
+          size={20}
+        />
+      )}
+    </View>
+  )
+}
